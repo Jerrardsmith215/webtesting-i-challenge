23f78c7a510ef85ec433f2a24418946b6bf2d70f
unknown <jerrardsmith215@gmail.com>
2019-04-25 22:33:56 -0400

    Jerrard Smith - Initial Commit - Repair function written + tested

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
 const enhancer = require('./enhancer.js');
+// test variables
+const sword = {
+    name: "Broad Sword",
+    atk: 10,
+    durability: 95
+};
+
 // test away!
+
+// Repair function test
+describe('repair function', () => {
+    it('sets durability key to a value of 100', () => {
+        const expected = { name: 'Broad Sword',
+        atk: 10, 
+        durability: 100};
+        const actual = enhancer.repair(sword)
+
+        expect(actual).toEqual(expected)
+    });
+});
+
+console.log('Hi')
