df7e5439c6297accc90ec4d18ff41430f8e57e35
unknown <jerrardsmith215@gmail.com>
2019-04-25 23:56:31 -0400

    Fail functionality written, tests passed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,6 +6,22 @@ const sword = {
     durability: 65
 };
 
+const staff1 = {
+    name: "Water Staff",
+    enhancement: 12,
+    durability: 77
+};
+const staff2 = {
+    name: "Air Staff",
+    enhancement: 15,
+    durability: 75
+};
+const staff3 = {
+    name: "Fire Staff",
+    enhancement: 18,
+    durability: 88
+};
+
 // test away!
 
 // Repair function test
@@ -29,3 +45,43 @@ describe('Succeed function', () => {
     });
 });
 
+// Fail function test
+describe('Fail function', () => {
+    // functions
+    const fail = (thing) => {
+        return {...thing,
+        durability: thing.durability - 5
+        }
+    }
+
+    const bigFail = (thing) => {
+        return {...thing,
+        durability: thing.durability - 10
+        }
+    }
+
+    const epicFail = (thing) => {
+        return { ... thing,
+        durability: thing.durability - 10,
+        enhancement: thing.enhancement - 1,
+        }
+    }
+
+    it('if enhancement level is < 15, durability - 5', () => {
+        const expected = fail(staff1);
+        const actual = enhancer.fail(staff1);
+        expect(expected).toEqual(actual);
+    });
+
+    it('if enhancement level is >= 15, durability - 10', () => {
+        const expected = bigFail(staff2);
+        const actual = enhancer.fail(staff2);
+        expect(expected).toEqual(actual);
+    });
+
+    it('if enhancement level is > 16, durability -10 & enhancement -1', () => {
+        const expected = epicFail(staff3);
+        const actual = enhancer.fail(staff3);
+        expect(expected).toEqual(actual);
+    });
+});
